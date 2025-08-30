8c77e2d872f2e6b458d9785c70d39a4495b2e43d
SantinoSchiavoni <2319332@ucc.edu.ar>
2025-08-29 23:27:28 -0300

    add GitHub Pages deploy workflow

--- lecaps-app/vite.config.ts
+++ lecaps-app/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'DBLecaps', // 👈 si el repo se llama "lecaps-app", poné '/lecaps-app/'
 })
+
